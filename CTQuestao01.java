8aa50968ea50d80257edbe5a8798064418e7d947
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-10-17 09:57:11 -0300

    praticando

---
+++ CTQuestao01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CTQuestao01{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Informe sua idade:");
+        int idade = scan.nextInt();
+
+        if (idade<16) {
+            System.out.println("Não pode votar");
+        }
+        else 
+        if(idade>=16 && idade<=17 || idade>65){
+            System.out.println("Votar facultativo");
+        }
+        else 
+        if(idade>=18 && idade<=65){
+            System.out.println("Votar obrigatorio");
+        }
+    }    
+}
